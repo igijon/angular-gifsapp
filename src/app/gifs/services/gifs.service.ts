dda027b74f950c262be25274a38d6516ea9c9ac4
Inma Gijón <igijoninf@gmail.com>
2022-05-31 13:03:11 +0200

    Controlar historial de búsquedas: vacíos, repetidos...

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,9 +13,17 @@ export class GifsService {
     return [...this._historial];
   }
 
-  buscarGifs( query: string ) {
-    this._historial.unshift( query );
-    console.log(this._historial);
+  buscarGifs( query: string = '') {
+
+    query = query.trim().toLocaleLowerCase();
+
+    if( !this._historial.includes(query) ) {
+
+      this._historial.unshift( query );
+      //Limito el historial a 10
+      this._historial = this._historial.splice(0,10);
+
+    }
 
   }
 }
